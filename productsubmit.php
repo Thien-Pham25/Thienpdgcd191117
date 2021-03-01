a537b9a30c73b95f184851c0fb2b935992d3a82a
Thien-Pham25 <pdthienvn@gmail.com>
2021-03-01 13:25:50 +0700

    Add files via upload

--- productsubmit.php
+++ productsubmit.php
@@ -7,26 +7,28 @@ user=iupvxhmesegzjq
 password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb
 sslmode=prefer");
 
-if ($connect === false) 
-{
-  die("ERROR: Could not connect to the database server!");
-} else 
-{
-  //echo ("Connect successfully!");
-  $product_name = $_POST['product-name'];
-  $product_price = $_POST['product-price'];
-  $product_amount = $_POST['product-amount'];
-}
-//echo ("Connect successfully!");
-$query = "INSERT INTO product (product_name, product_price, product_amount) 
-VALUES('$product_name', '$product_price', '$product_amount');";
-$result = pg_query($connect, $query);
-if ($result) 
-{
-  echo "<script>alert('Record added succesfully!, Refresh');</script>";
-  header('refresh: 3; url=productform.php');
-} else 
-{
-  echo ("ERROR + $query") . pg_errormessage($query);
-}
-pg_close($connect);
+  if ($connect === false) 
+    {
+      die("ERROR: Could not connect to the database server!");
+    } 
+  else 
+    {
+      //echo ("Connect successfully!");
+      $product_name = $_POST['product-name'];
+      $product_price = $_POST['product-price'];
+      $product_amount = $_POST['product-amount'];
+    }
+      //echo ("Connect successfully!");
+      $query = "INSERT INTO product (product_name, product_price, product_amount) 
+      VALUES('$product_name', '$product_price', '$product_amount');";
+      $result = pg_query($connect, $query);
+    if ($result) 
+    {
+      echo "<script>alert('Record added succesfully!, Refresh');</script>";
+      header('refresh: 3; url=productform.php');
+    } 
+    else 
+    {
+      echo ("ERROR + $query") . pg_errormessage($query);
+    }
+    pg_close($connect);
