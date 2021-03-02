bb6b6a9ac7549ab6325c4d2e29cdaf442aa472ce
Thien-Pham25 <pdthienvn@gmail.com>
2021-03-02 16:08:01 +0700

    Update productdelete.php

--- productdelete.php
+++ productdelete.php
@@ -7,14 +7,14 @@ user=iupvxhmesegzjq
 password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb
 sslmode=prefer");
 
-    $connect = new pg_connect($host, $dbname, $port, $user, $password, $sslmode);
+    $connect = new mysqli($host, $dbname, $port, $user, $password, $sslmode);
     if ($connect->connect_error) 
     {
         die("Connection failed: " . $conn->connect_error);
     }
-    $pg_connect = "DELETE FROM product WHERE product_id=1";
+    $sql = "DELETE FROM product WHERE product_id=1";
 
-    if ($conn->query($pg_connect) === TRUE) 
+    if ($conn->query($sql) === TRUE) 
     {
         echo "Record deleted successfully";
     } 
