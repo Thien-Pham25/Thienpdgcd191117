9d4b4cc04a288034b4e69a82098badf8d6d3b70a
Thien-Pham25 <pdthienvn@gmail.com>
2021-03-02 16:49:55 +0700

    Update productdelete.php

--- productdelete.php
+++ productdelete.php
@@ -6,7 +6,7 @@ port=5432
 user=iupvxhmesegzjq
 password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb");
 
-    $connect = new mysqli_connect($host, $dbname, $user, $password);
+    $connect = new mysqli($host, $dbname, $user, $password);
     if ($connect->connect_error) 
     {
         die("Connection failed: " . $conn->connect_error);
