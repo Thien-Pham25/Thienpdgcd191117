b39495305223638ed58b17a13ab600a5f6466538
Thien-Pham25 <pdthienvn@gmail.com>
2021-03-02 16:39:07 +0700

    Update productdelete.php

--- productdelete.php
+++ productdelete.php
@@ -4,10 +4,9 @@ $connect = pg_connect
 dbname=ddo3aus679kv6d
 port=5432
 user=iupvxhmesegzjq
-password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb
-sslmode=prefer");
+password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb");
 
-    $connect = new mysqli_pg_connect($host, $dbname, $port, $user, $password, $sslmode);
+    $connect = new mysqli_pg_connect($host, $dbname, $user, $password);
     if ($connect->connect_error) 
     {
         die("Connection failed: " . $conn->connect_error);
