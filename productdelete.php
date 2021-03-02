7076a5e3f18b245098d85fef964eae3852915ae3
Thien-Pham25 <pdthienvn@gmail.com>
2021-03-02 17:15:18 +0700

    Add files via upload

--- productdelete.php
+++ productdelete.php
@@ -1,28 +1,18 @@
 <?php
-$connect = pg_connect
-("host=ec2-52-7-168-69.compute-1.amazonaws.com
-dbname=ddo3aus679kv6d
-port=5432
-user=iupvxhmesegzjq
-password=0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb");
+    $host = "ec2-52-7-168-69.compute-1.amazonaws.com";
+    $dbname = "ddo3aus679kv6d";
+    $user = "iupvxhmesegzjq";
+    $password = "0d633c625519042f54ca84dc367630ba07f2028784c6a03108b94aa49376a7bb";
+    $connection_string = "host=". $host . " port=5432 dbname=" . $dbname . " user=" . $user. " password=".$password;
+    $dbServer = pg_connect($connection_string);
 
-    $connect = new pg_connect($host, $dbname, $user, $password);
-    if ($connect->connect_error) 
-    {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $pg_connect = "DELETE FROM product WHERE product_id=1";
+    $id = 1;
 
-    if ($conn->query($pg_connect) === TRUE) 
-    {
-        echo "Record deleted successfully";
-    } 
-    else 
-    {
-        echo "Error deleting record: " . $conn->error;
-    }
+    $query = "DELETE FROM product WHERE product_id = " . $id;
+    
+    $result = pg_query($dbServer, $query);
      
-    $connect->close();
+    pg_close($dbServer)
     ?>
 
 
